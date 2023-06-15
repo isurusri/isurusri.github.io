1847082b8ecaa5185885b775f8929710d76f9586
Isuru Srimal <isuru.srimal258@gmail.com>
2023-06-15 21:55:19 +0530

    boilerpate created

---
+++ src/components/Intro.tsx
@@ -0,0 +1,17 @@
+function Intro() {
+    return (
+        <>
+            <div className='flex items-center justify-center flex-col text-center pt-20 pb-6'>
+                <h1 className='text-4xl md:text-7xl mb-1 md:mb-3 font-bold'>ISURU</h1>
+                <p className='text-base md:text-xl mb-3 font-medium'>Software Engineer</p>
+                <p className='text-sm max-w-xl mb-6 font-bold'>
+                    Software Engineer with 3 years of industry experience capable of leading large engineering teams to achieve set goals on a given time-frame, coming up with innovative solutions.
+                    <br/>
+                    Experience working for a diverse clientele with strong Java, Database management and NodeJs skills.
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Spring Boot Microservice',
+        imgUrl: '',
+        stack: ['SpringBoot', 'MySQL', 'Docker'],
+        link: ''
+    },
+    {
+        title: 'Blog App',
+        imgUrl: '',
+        stack: ['NodeJs', 'MongoDB', 'EJS'],
+        link: ''
+    },
+    {
+        title: 'ToDo App',
+        imgUrl: '',
+        stack: ['NodeJs', 'MongoDB', 'Angular'],
+        link: ''
+    }
+]
