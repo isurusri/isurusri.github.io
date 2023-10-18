6847be2406c0ce727efc3177c532dcd0d54e1e08
Isuru Srimal Abeykoon <isuru.srimal258@gmail.com>
2023-10-18 23:25:52 +0530

    image url fixed

--- src/data/portfolio.tsx
+++ src/data/portfolio.tsx
@@ -1,20 +1,23 @@
 export default [
-    {
-        title: 'Spring Boot Microservice',
-        imgUrl: 'https://azpanel.azilen.com/uploads/everything_must_know_spring_boot_application_scratch_12_8c4e62d4fe.jpg',
-        stack: ['SpringBoot', 'MySQL', 'Docker'],
-        link: 'https://github.com/isurusri/Spring-Boot-Microservices-'
-    },
-    {
-        title: 'Blog App',
-        imgUrl: 'https://bloggernz.co.nz/wp-content/uploads/2022/04/image-1-2.jpg',
-        stack: ['NodeJs', 'MongoDB', 'React'],
-        link: 'https://github.com/isurusri/node-crash-BlogApp'
-    },
-    {
-        title: 'ToDo App',
-        imgUrl: 'https://clickup.com/blog/wp-content/uploads/2019/01/to-do-list-apps.png',
-        stack: ['NodeJs', 'MongoDB', 'Angular'],
-        link: 'https://github.com/isurusri/Todo-MEAN-App'
-    }
-]
+  {
+    title: "Spring Boot Microservice",
+    imgUrl:
+      "https://www.cleo.com/sites/default/files/2018-10/microservices-architecture.jpg",
+    stack: ["SpringBoot", "MySQL", "Docker"],
+    link: "https://github.com/isurusri/Spring-Boot-Microservices-",
+  },
+  {
+    title: "Blog App",
+    imgUrl:
+      "https://cdn4.wpbeginner.com/wp-content/uploads/2018/07/whatisblog.png",
+    stack: ["NodeJs", "MongoDB", "React"],
+    link: "https://github.com/isurusri/node-crash-BlogApp",
+  },
+  {
+    title: "ToDo App",
+    imgUrl:
+      "https://clickup.com/blog/wp-content/uploads/2019/01/to-do-list-apps.png",
+    stack: ["NodeJs", "MongoDB", "Angular"],
+    link: "https://github.com/isurusri/Todo-MEAN-App",
+  },
+];
